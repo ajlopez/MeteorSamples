8d6694c0926f7e3a3211fb7a07b262c9f97d54cb
ajlopez <webmaster@ajlopez.com>
2014-02-15 16:23:04 -0300

    Adding routing using backbone and conditional templates

--- bootstrap/bootstrap.js
+++ bootstrap/bootstrap.js
@@ -1,15 +1,40 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to bootstrap.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    Template.home.showHome = function() {
+        return Session.get("operation") == 'showHome';
     }
-  });
+    
+    Template.about.showAbout = function() {
+        return Session.get("operation") == 'showAbout';
+    }
+    
+    Template.contact.showContact = function() {
+        return Session.get("operation") == 'showContact';
+    }
+
+    var Router = Backbone.Router.extend({
+      routes: {
+        "":                 "home", //this will be http://your_domain/
+        "about":            "about",  // http://your_domain/about
+        "contact":          "contact"  // http://your_domain/about
+      },
+
+      home: function() {
+        Session.set('operation', 'showHome');
+      },
+
+      about: function() {
+        Session.set('operation', 'showAbout');
+      },
+
+      contact: function() {
+        Session.set('operation', 'showContact');
+      }
+    });
+    
+    var app = new Router;
+    Meteor.startup(function () {
+      Backbone.history.start({pushState: true});
+    });
 }
 
 if (Meteor.isServer) {
