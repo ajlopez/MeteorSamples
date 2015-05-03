6880594de7a83b72dc34ca65f27e010b4bee83a2
ajlopez <webmaster@ajlopez.com>
2015-05-03 08:20:35 -0300

    To do list in simple-todos

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,18 +1,12 @@
+// simple-todos.js
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
   });
 }
 
