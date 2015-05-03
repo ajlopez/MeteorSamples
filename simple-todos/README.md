20e7f2315aa8dcfc88e48c92da32f40839be9b0c
ajlopez <webmaster@ajlopez.com>
2015-05-03 08:44:54 -0300

    Checked, delete task

---
+++ simple-todos/README.md
@@ -0,0 +1,6 @@
+
+# Simple To Do Application
+
+From https://www.meteor.com/try
+
+Running at http://mytdsample.meteor.com/
--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,13 +1,43 @@
 // simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
+// simple-todos.js
 if (Meteor.isClient) {
   // This code only runs on the client
-  Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
-  });
+    Template.body.helpers({
+        tasks: function () {
+          return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    });
+
+    Template.body.events({
+      "submit .new-task": function (event) {
+        // This function is called when the new task form is submitted
+
+        var text = event.target.text.value;
+
+        Tasks.insert({
+          text: text,
+          createdAt: new Date() // current time
+        });
+
+        // Clear form
+        event.target.text.value = "";
+
+        // Prevent default form submit
+        return false;
+      }
+    });
+
+    Template.task.events({
+      "click .toggle-checked": function () {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      },
+      "click .delete": function () {
+        Tasks.remove(this._id);
+      }
+    });
 }
 
 if (Meteor.isServer) {
